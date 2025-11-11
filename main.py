2f34f502c6468bbc16bb7712505101f7f870cd52
Elliot Nou <34079960+woochachino@users.noreply.github.com>
2025-11-11 21:55:36 +0000

    idk

--- main.py
+++ main.py
@@ -10,39 +10,7 @@ def read_csv(filename):
         list.append(i[0])
     return list
 
-def compare_portfolio_to_benchmark(portfolio_dict, start, end):
-    """
-    Takes in a portfolio, compares its return within start and end dates
-    to the benchmark average return. For testing purposes.
-    """
-
-    tickers = list(portfolio_dict.keys())
-    weights = np.array(list(portfolio_dict.values()))
-
-    data = yf.download(tickers, start=start, end=end, progress=False)["Close"]
-    daily_ret = data.pct_change().dropna().dot(weights)
-    portfolio_total = (1 + daily_ret).prod() - 1
-
-    tsx = yf.download("^GSPTSE", start=start, end=end, progress=False)["Close"]
-    spx = yf.download("^GSPC", start=start, end=end, progress=False)["Close"]
-
-
-    if isinstance(tsx, pd.DataFrame):
-        tsx = tsx.squeeze()
-    if isinstance(spx, pd.DataFrame):
-        spx = spx.squeeze()
-
-    tsx_total = tsx.iloc[-1] / tsx.iloc[0] - 1
-    spx_total = spx.iloc[-1] / spx.iloc[0] - 1
-    benchmark_total = (tsx_total + spx_total) / 2
-
-    diff = portfolio_total - benchmark_total
-
-    return {
-        "Portfolio_Total_Return_%": round(float(portfolio_total * 100), 2),
-        "Benchmark_Avg_Return_%": round(float(benchmark_total * 100), 2),
-        "Difference_%": round(float(diff * 100), 2)
-    }
+      
 
 
 def check_ticker(list):
@@ -82,16 +50,16 @@ def check_ticker(list):
 
     return valid_tickers, invalid_tickers
 
+def blended_benchmark(start, end):
+    data = yf.download(["^GSPC", "^GSPTSE"], start=start, end=end)["Close"]
+    rets = data.pct_change().dropna()
+    blended = rets.mean(axis=1)
+    return blended.rename("Benchmark")
+    
 def score_data(valid_tickers):
     start="2025-05-15"
     end="2025-11-15"
 
-    def blended_benchmark(start, end):
-        data = yf.download(["^GSPC", "^GSPTSE"], start=start, end=end)["Close"]
-        rets = data.pct_change().dropna()
-        blended = rets.mean(axis=1)
-        return blended.rename("Benchmark")
-    
 
     def get_sector_safe(ticker):
         try:
@@ -116,7 +84,8 @@ def score_data(valid_tickers):
     
 
     for i in valid_tickers:
-        stock_ret = yf.download(i, start=start, end=end)["Close"].pct_change().dropna()
+        stock_ret = yf.download(i, start=start, end=end, auto_adjust=True)["Close"].pct_change().dropna()
+
         stock_ret.name = i 
         df = pd.concat([stock_ret, bench], axis=1).dropna()
         if len(df) < 5:
@@ -158,6 +127,44 @@ def score_data(valid_tickers):
         else:
             remaining.append(i)
     return remaining, final'''
+
+
+def compare_portfolio_to_benchmark(portfolio_dict, start, end):
+    """
+    Takes in a portfolio, compares its return within start and end dates
+    to the benchmark average return. For testing purposes.
+    """
+
+    tickers = list(portfolio_dict.keys())
+    weights = np.array(list(portfolio_dict.values()))
+
+    data = yf.download(tickers, start=start, end=end, progress=False, auto_adjust=True)["Close"]
+
+    daily_ret = data.pct_change().dropna().dot(weights)
+    portfolio_total = (1 + daily_ret).prod() - 1
+
+    tsx  = yf.download("^GSPTSE", start=start, end=end, progress=False, auto_adjust=True)["Close"]
+    spx  = yf.download("^GSPC", start=start, end=end, progress=False, auto_adjust=True)["Close"]
+
+
+    if isinstance(tsx, pd.DataFrame):
+        tsx = tsx.squeeze()
+    if isinstance(spx, pd.DataFrame):
+        spx = spx.squeeze()
+
+    tsx_total = tsx.iloc[-1] / tsx.iloc[0] - 1
+    spx_total = spx.iloc[-1] / spx.iloc[0] - 1
+    bench = blended_benchmark(start, end)
+    benchmark_total = (1 + bench).prod() - 1
+
+
+    diff = portfolio_total - benchmark_total
+
+    return {
+        "Portfolio_Total_Return_%": round(float(portfolio_total * 100), 2),
+        "Benchmark_Avg_Return_%": round(float(benchmark_total * 100), 2),
+        "Difference_%": round(float(diff * 100), 2)
+    }
 
 
 def main():
